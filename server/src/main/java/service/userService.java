f93b7bdc5931678bc1eab615ca3195fc24a15546
nicholasA113 <nde1222@byu.edu>
2025-02-19 18:42:23 -0700

    Phase 3 Commit #6

--- server/src/main/java/service/userService.java
+++ server/src/main/java/service/userService.java
@@ -1,19 +1,50 @@
 package service;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
 import service.RequestResultRecords.*;
+
+import java.util.UUID;
 
 public class userService {
 
     public RegisterResult registerUser(RegisterRequest r){
-
+        UserDataDAO userDataDAO = new UserDataDAO();
+        UserData userdata = new UserData(r.username(), r.password(), r.email());
+        UserData user = userDataDAO.getUser(r.username());
+        if (user != null){
+            return null;
+        }
+        userDataDAO.insertUser(userdata);
+        AuthDataDAO authDataDAO = new AuthDataDAO();
+        String authToken = UUID.randomUUID().toString();
+        authDataDAO.createAuth(new AuthData(authToken, r.username()));
+        return new RegisterResult(r.username(), authToken);
     }
 
     public LoginResult login(LoginRequest l){
-
+        UserDataDAO userDataDAO = new UserDataDAO();
+        UserData user = userDataDAO.getUser(l.username());
+        if (user == null){
+            return null;
+        }
+        AuthDataDAO authDataDAO = new AuthDataDAO();
+        String authToken = UUID.randomUUID().toString();
+        authDataDAO.createAuth(new AuthData(authToken, l.username()));
+        return new LoginResult(l.username(), authToken);
     }
 
     public LogoutResult logout(LogoutRequest l){
-
+        AuthDataDAO authDataDAO = new AuthDataDAO();
+        AuthData authToken = authDataDAO.getAuth(l.authToken());
+        if (authToken != null){
+            authDataDAO.deleteAuth(l.authToken());
+            return new LogoutResult();
+        }
+        else{
+            return null;
+        }
     }
 
 }

61e7773389283bdba4e0f87228da3db310993a71
nicholasA113 <nde1222@byu.edu>
2025-02-20 16:03:21 -0700

    Phase 3 Commit #9

--- server/src/main/java/service/httpHandlers/RegisterHandler.java
+++ server/src/main/java/service/httpHandlers/RegisterHandler.java
@@ -14,14 +14,17 @@ public class RegisterHandler {
         String registerRequestJson = request.body();
         RequestResult.RegisterRequest registerRequest = serializer.fromJson(registerRequestJson,
                 RequestResult.RegisterRequest.class);
+        if (registerRequest.username() == null || registerRequest.password() == null ||
+        registerRequest.email() == null){
+            response.status(400);
+            return "{Bad Request: Username, password, and/or email are empty}";
+        }
         UserService userservice = new UserService();
         RequestResult.RegisterResult registerResult = userservice.registerUser(registerRequest);
-
         if (registerResult == null){
             response.status(403);
-            return "{'Message': 'Username is already taken.'}";
+            return "{Username is already taken.}";
         }
-
         response.status(200);
         return serializer.toJson(registerResult);
     }

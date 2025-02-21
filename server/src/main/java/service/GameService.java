27df25106789b7ba8408ba6920039c740c176410
nicholasA113 <nde1222@byu.edu>
2025-02-21 16:15:13 -0700

    Phase 3 Commit #12

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,21 @@
 package service;
 
+import dataaccess.AuthDataDAO;
+import dataaccess.GameDataDAO;
+import model.AuthData;
+import model.GameData;
 import service.RequestResult.*;
 
 public class GameService {
 
     public CreateGameResult createGame(CreateGameRequest c){
-
+        AuthDataDAO authDataDAO = new AuthDataDAO();
+        AuthData authToken = authDataDAO.getAuth(c.authToken());
+        if (authToken.username() == null){
+            return null;
+        }
+        GameDataDAO gameDataDAO = new GameDataDAO();
+        gameDataDAO.createGame(c.gameName());
     }
 
     public JoinGameResult joinGame(JoinGameRequest j){

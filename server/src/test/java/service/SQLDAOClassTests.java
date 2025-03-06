e6b4224a6e42ac72255837024cd0e4580ee40b7a
nicholasA113 <nde1222@byu.edu>
2025-03-06 12:14:43 -0700

    Phase 4 Commit #5

--- server/src/test/java/service/SQLDAOClassTests.java
+++ server/src/test/java/service/SQLDAOClassTests.java
@@ -6,6 +6,8 @@ import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.*;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SQLDAOClassTests {
@@ -15,14 +17,25 @@ public class SQLDAOClassTests {
     public void authDataTableCreationSuccess() throws DataAccessException {
         AuthData authData = new AuthData( "authToken123", "nicholasUsername");
         SQLAuthDataDAO sqlAuthDataDAO = new SQLAuthDataDAO();
+
         sqlAuthDataDAO.createAuth(authData);
 
         try (var conn = DatabaseManager.getConnection()){
-            String queryStatement = "SELECT authToken, username FROM authData WHERE authToken = 'authToken123'";
-            try (var preparedStatement = conn.prepareStatement(queryStatement)){
+            String queryStatement = "SELECT authToken, username FROM authData WHERE authToken = ?";
+            var preparedStatement = conn.prepareStatement(queryStatement);
+            preparedStatement.setString(1, "authToken123");
+            try (var result = preparedStatement.executeQuery()){
+                String authTokenResult = result.getString("authToken");
+                String usernameResult = result.getString("username");
 
+                Assertions.assertEquals("authToken123", authTokenResult,
+                        "AuthTokens do not match");
+                Assertions.assertEquals("nicholasUsername", usernameResult,
+                        "Usernames do not match");
             }
         }
-
+        catch (SQLException e){
+            throw new DataAccessException("Error creating auth data: " + e.getMessage());
+        }
     }
 }

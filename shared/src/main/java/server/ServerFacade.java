56c39452756b707f0296e8145bdfcf70cab96324
nicholasA113 <nde1222@byu.edu>
2025-03-12 22:50:21 -0600

    phase 5 commit #2

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,24 +1,58 @@
 package server;
 
+import java.net.URI;
+import java.net.URL;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url){
+        serverUrl = url;
+    }
 
     public RegisterResult register(RegisterRequest request){
         var path = "/user";
-        return this.makeRequest("POST", path, request, RegisterRequest.class);
+        return this.makeRequest("POST", path, request, RegisterResult.class);
     }
 
     public LoginResult login(LoginRequest request){
         var path = "/session";
-        return this.makeRequest("POST", path, request, LoginRequest.class);
+        return this.makeRequest("POST", path, request, LoginResult.class);
     }
 
-    public LogoutResult logout(){
+    public LogoutResult logout(LogoutRequest request){
         var path = "/session";
         return this.makeRequest("DELETE", path, request, LogoutResult.class);
     }
 
-    private<T> T makeRequest(String method, String path, Object request, Class<T> responseClass){
+    public CreateGameResult createGame(CreateGameRequest request){
+        var path = "/game";
+        return this.makeRequest("POST", path, request, CreateGameResult.class);
+    }
 
+    public JoinGameResult joinGame(JoinGameRequest request){
+        var path = "/game";
+        return this.makeRequest("PUT", path, request, JoinGameResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request){
+        var path = "/game";
+        return this.makeRequest("GET", path, request, ListGamesResult.class);
+    }
+
+    public ClearResult clear(ClearRequest request){
+        var path = "/db";
+        return this.makeRequest("DELETE", path, request, ClearResult.class);
+    }
+
+    private<T> T makeRequest(String method, String path, Object request, Class<T> responseClass){
+        try{
+            URL url = (new URI(serverUrl+path)).toURL();
+        }
+        catch (Exception e){
+
+        }
     }
 
 }
